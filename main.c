1e598613f21481d72f0bce5454f2028eba10a888
MexaLuch <43755779+MexaLuch@users.noreply.github.com>
2024-10-15 00:05:51 +0300

    Added: square_validation, square_intersection, square_count_t

--- main.c
+++ main.c
@@ -11,7 +11,10 @@ typedef struct {
     double x3, y3;
     double x4, y4;
     char in_circle;
+    char valid;
 } square_t;
+
+typedef unsigned long long int square_count_t;
 
 // Определение вершин квадрата по центру, размеру и углу поворота
 square_t square_calc(square_t sq) {
@@ -36,8 +39,8 @@ square_t square_calc(square_t sq) {
 }
 
 // Функция проверки, попадания вершины в круг
-void points_in_circle(square_t *square, int square_count) {
-    for (long int i = 0; i < square_count; i++)
+void points_in_circle(square_t *square, square_count_t square_count) {
+    for (square_count_t i = 0; i < square_count; i++)
     {
         double r1 = sqrt((square[i].x1 * square[i].x1) + (square[i].y1 * square[i].y1));
         double r2 = sqrt((square[i].x2 * square[i].x2) + (square[i].y2 * square[i].y2));
@@ -55,17 +58,76 @@ void points_in_circle(square_t *square, int square_count) {
     }
 }
 
+// Функция проверки, пересечения квадратов
+char square_intersection(square_t square_1, square_t square_2) {
+
+    double square_2_points[] = {square_2.x1, square_2.y1,
+                                square_2.x2, square_2.y2,
+                                square_2.x3, square_2.y3,
+                                square_2.x4, square_2.y4};
+
+    for (char i = 0; i < 4; i++)
+    {
+        // Перемещение точки в систему координат, где квадрат центрирован в начале координат
+        double x_translocated = square_2_points[i] - square_1.x;
+        double y_translocated = square_2_points[i+1] - square_1.y;
+
+        double radians = square_1.theta * (M_PI / 180.0);
+
+        // Обратное вращение точки на угол квадрата
+        double rotatedX = x_translocated * cos(-radians) - y_translocated * sin(-radians);
+        double rotatedY = x_translocated * sin(-radians) + y_translocated * cos(-radians);
+
+        // Проверка, находится ли точка вне неповернутого квадрата
+        double halfSide = square_1.size / 2.0;
+        if (fabs(rotatedX) > halfSide || fabs(rotatedY) > halfSide) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+// Функция проверки, пересечения квадратов
+void square_validation(square_t *square, square_count_t square_count) {
+    double diag = sqrt(2)*square[1].size;
+    for (square_count_t i = 0; i < square_count; i++)
+    {
+        for (square_count_t j = 0; j < square_count; j++)
+        {
+            // Установлен ли квадрат j
+            if (square[j].valid)
+            {
+                // Способны ли квадраты i и j пересечься
+                if (sqrt(pow(square[i].x - square[j].x, 2) + pow(square[i].y - square[j].y, 2)) < diag)
+                {
+                    //Пересекаются ли они?
+                    if (square_intersection(square[i], square[j]))
+                    {
+                        break;
+                    }
+                }
+            }
+
+            // Если сравнили со всеми и пересечений не было, то устанавливаем квадрат
+            if (j == square_count-1)
+            {
+                square[i].valid = 1;
+            }
+        }
+    }
+}
+
 int main(void) {
     //srand(11);       //TODO: ДОПИЛИТЬ ВРЕМЯ
 
-    double square_size = 0.0025;
-    long int square_count = (int) (((1*1) / (square_size*square_size) ) * 1000);  // замостить квадрат 1x1 1000 раз квадратами размером size*size
+    double square_size = 0.25;
+    square_count_t square_count = (int) (((1*1) / (square_size*square_size) ) * 1000);  // замостить квадрат 1x1 1000 раз квадратами размером size*size
 
     printf("All:%d\n",square_count);
 
     square_t *square;
     square = malloc(sizeof(square_t)*square_count);
-    for (long int i = 0; i < square_count; i++)
+    for (square_count_t i = 0; i < square_count; i++)
     {
         // генерация
         square[i].size   = square_size;
@@ -74,12 +136,13 @@ int main(void) {
         square[i].theta  = (rand() / (double)RAND_MAX) * 0.5 * M_PI; //0  - PI/2
 
         square[i] = square_calc(square[i]);
+        square[i].valid = 0;
     }
 
     points_in_circle(square, square_count);
 
-    long int in_circle_count = 0;
-    for (long int i = 0; i < square_count; i++)
+    square_count_t in_circle_count = 0;
+    for (square_count_t i = 0; i < square_count; i++)
     {
         if (1 == square[i].in_circle)
         {
@@ -87,9 +150,23 @@ int main(void) {
         } 
     }
 
+    square_validation(square, square_count);
+
+    square_count_t valid_squers = 0;
+    for (square_count_t i = 0; i < square_count; i++)
+    {
+        if (1 == square[i].valid)
+        {
+            valid_squers++;
+        } 
+    }
+
 
     printf("in_circle:%d\n",in_circle_count);
-    printf("PI:%f\n", 4.0 * in_circle_count / square_count);
+    printf("PI:%f\n\n", 4.0 * in_circle_count / square_count);
+
+    printf("valid_squers:%d\n", valid_squers);
+    printf("S:%f\n", (double)valid_squers * (square_size*square_size));
 
 
     free(square);
